e4f7ffaf6fdb22de25b891b64d572be0d7ecc0ca
beryl0202 <tnguy698@ucsc.edu>
2025-04-02 17:22:50 -0700

    Added hello world program
    This repository isn't empty anymore!

---
+++ Hello-world.py
@@ -0,0 +1,17 @@
+#This program will write
+# Hello World!
+
+#This function tells the computer to print
+#Hello World!
+def PrintHelloWOrld():
+	print("Hello World!")
+
+
+
+def main():
+	PrintHelloWOrld()
+
+if __main__=='__main__':
+	main()
+
+	
