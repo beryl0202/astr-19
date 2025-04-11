a1ed7dff86516cf2e6d49d41cc92bd91d35ce26c
beryl0202 <tnguy698@ucsc.edu>
2025-04-10 21:51:35 -0700

    added session1 and session2

---
+++ Session1Prompt.py
@@ -0,0 +1,1 @@
+print("Thai Nguyen \n My favorite movie is Skyfall and my favorite food is Taiwanese popcorn chicken.")
---
+++ Session2Prompt.py
@@ -0,0 +1,18 @@
+#Initialize two float and 2 int
+flo1 = 0.5
+flo2 = 0.10
+int1 = 20
+int2 = 5 
+
+num1 = flo1 + flo2
+num2 = int1 - int2
+num3 = flo2*int2 
+#Print sum of two floats
+print("0.5 + 0.10 =", num1)
+print(type(num1))
+#Print difference of two int
+print("int1 - int2 = ", num2)
+print(type(num2))
+#Print product of a float and int
+print("0.10 * 5 = ", num3)
+print(type(num3))
